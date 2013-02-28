f2facd1136c4c5bbddbca5009a726da81c96a523
splurgy <splurgy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-28 02:09:30 +0000

    updated version number
    git-svn-id: https://plugins.svn.wordpress.org/splurgy-wp-plugin/trunk@674383 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- WordpressInit.php
+++ WordpressInit.php
@@ -4,7 +4,7 @@
    Plugin URI: https://github.com/splurgy/plugins/
    Description: This plugin will allow users to easily add offers at the end of 
    their post
-   Version: 1.1.1
+   Version: 1.2.0
    Author: Splurgy
    Author URI: http://www.splurgy.com
    License: MIT
